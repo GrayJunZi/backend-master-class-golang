1ce9614ce25b1cc4f9a91c10add5ffc07d47a3cc
GrayJunZi <GrayJunZi@outlook.com>
2023-04-28 13:15:21 +0800

    十二、使用 Viper 从文件或环境变量中加载配置

--- main.go
+++ main.go
@@ -6,18 +6,18 @@ import (
 
 	"github.com/grayjunzi/backend-master-class-golang/api"
 	db "github.com/grayjunzi/backend-master-class-golang/db/sqlc"
+	"github.com/grayjunzi/backend-master-class-golang/util"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:root@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +25,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server:", err)
 	}
